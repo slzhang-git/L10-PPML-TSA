f5af964187c4b8d790c6a354ae39db50f7af7b28
slzhang-git <92975752+slzhang-git@users.noreply.github.com>
2022-12-21 13:59:57 +0100

    Update execute_baseline.sh
    only for purpose of electrical appliance classification

--- src/bash_scripts/execute_baseline.sh
+++ src/bash_scripts/execute_baseline.sh
@@ -10,9 +10,7 @@ echo "=================================================="
 echo "============= Start bash execution ==============="
 echo "=================================================="
 
-DATASETS=('AsphaltPavementType' 'AsphaltRegularity' 'CharacterTrajectories' 'Crop' 'ECG5000' \
-            'ElectricDevices' 'FaceDetection' 'FordA' 'HandOutlines' 'MedicalImages' 'MelbournePedestrian' \
-            'NonInvasiveFetalECGThorax1' 'PhalangesOutlinesCorrect' 'Strawberry' 'UWaveGestureLibraryAll' 'Wafer')
+DATASETS=('ElectricDevices')
 
 MODELS=('AlexNet')
 
